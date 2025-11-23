4e71a0c79e40c33c33a735d8297cca6d6f8f1bad
Karissa <csr24002@uconn.edu>
2025-11-22 23:39:04 -0500

    added menu items and header

--- final-project.R
+++ final-project.R
@@ -6,15 +6,23 @@ library(shinydashboard)
 gen_ai <- read.csv("generative_ai_misinformation_dataset.csv")
 
 ui <- dashboardPage(
-  dashboardHeader(title = "Generative AI Misinformation Research"),
+  dashboardHeader(
+    titleWidth = 400,
+    title = "Generative AI Misinformation Research"),
   dashboardSidebar(
+    width = 350,
     sidebarMenu(
-      menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard"))
+      menuItem("Sample of Data", tabName = "data", icon = icon("database")),
+      menuItem("Detected Gen AI Misinformation Around the World", tabName = "map", icon = icon("map-location-dot")),
+      menuItem("Misinformation Detection Frequency", tabName = "histogram", icon = icon("chart-column")),
+      menuItem("Factors of Misinformation Detection", tabName = "boxplot", icon = icon("chart-line"))
     )
   ),
   dashboardBody(
     fluidRow(
-      box()
+      box(
+        title = ""
+      )
     )
   )
 )
